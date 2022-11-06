4cb082860c16292812d5fe28740f6cb0425ec467
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-11-06 18:11:38 -0500

    save all changes

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask 
 
+#factory
 def create_app(): 
     app = Flask(__name__)
+    
+    from . import pet
+    app.register_blueprint(pet.bp)
 
     @app.route('/')
     def hello(): 
         return 'Hello, PetFax! Mike should make less typos'
-
+    
+    #return the app
     return app
